3fbcd725714e53fcf3262d1c4c4fa37e057882ce
Rupesh Maharana <rupesh.m@inspironlabs.com>
2023-12-27 17:11:30 +0530

    Java App to deploy on jenkins

---
+++ java-programs-jenkins/Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("This is a java Programme");
+         System.out.println("Output from java programme");
+          System.out.println(new Date());
+    }
+}
